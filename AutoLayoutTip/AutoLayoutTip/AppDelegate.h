860d6aa8658608bb589f757d32fe26124d85c45e
hongqingWang <272338444@qq.com>
2018-08-10 15:58:24 +0800

    添加代码
    添加代码

---
+++ AutoLayoutTip/AutoLayoutTip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutTip
+//
+//  Created by 王红庆 on 2018/8/10.
+//  Copyright © 2018年 王红庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
